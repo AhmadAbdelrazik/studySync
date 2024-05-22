219bd0bd4f6d3bd5477c4db6c9885208d6d8a368
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-05-22 06:31:58 +0300

    Fixed Some errors

--- src/Controller/courses.js
+++ src/Controller/courses.js
@@ -1,24 +1,28 @@
 const Course = require("../Model/courses");
 const validator = require("../utils/courseValidator");
+const httpStatus = require("../utils/HttpStatus");
 
 const getCourses = (req, res) => {
   Course.courses()
-    .then((courses) => res.status(200).send(courses))
-    .catch((e) => res.status(400).send(e));
+    .then((courses) => res.status(httpStatus.OK).send(courses))
+    .catch((e) => res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e));
 };
 
 const addCourse = (req, res) => {
   let crs = new Course(req.body.name);
 
   crs.addCourse()
-    .then((m) => res.status(200).send(m))
-    .catch((e) => res.status(401).send(e));
+    .then((m) => res.status(201).send(m))
+    .catch(e => {
+      res.status(401).send(e);
+      console.log(e);
+    })
 };
 
 const courseParam = async (req, res, next, val) => {
   try {
     const courses = await Course.courses();
-    const idx = courses.find(obj => obj.name === val);
+    const idx = courses.find((obj) => obj.name === val);
     if (idx == -1) {
       return res.status(404).send("Course Not found");
     } else {
@@ -28,7 +32,6 @@ const courseParam = async (req, res, next, val) => {
   } catch (err) {
     res.status(401).send(err);
   }
-
 };
 
 module.exports = {
--- src/Model/courses.js
+++ src/Model/courses.js
@@ -1,58 +1,71 @@
-const fs = require('fs/promises');
-const path = require('path');
-const coursesPath = path.join(path.dirname(require.main.filename), "Data", "courses.json");
-const questionsPath = path.join(path.dirname(require.main.filename), "Data", "Questions");
-module.exports = class Courses{
+const fs = require("fs/promises");
+const path = require("path");
+const coursesPath = path.join(
+  path.dirname(require.main.filename),
+  "Data",
+  "courses.json"
+);
+const questionsPath = path.join(
+  path.dirname(require.main.filename),
+  "Data",
+  "Questions"
+);
+module.exports = class Courses {
   constructor(nm) {
     this.name = nm;
   }
 
   async addCourse() {
-    return new Promise (async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       try {
         // 1) load the courses from database.
         const data = await fs.readFile(coursesPath);
         const courses = JSON.parse(data);
-        
         // Parse The Name Format
-        let courseName = req.body.name.split(" ");
-        courseName = courseName.map(w => w.toLowerCase());
+        let courseName = this.name.split(" ");
+        courseName = courseName.map((w) => w.toLowerCase());
         courseName = courseName.join("_");
-    
+
         // 2) Check If Course is there.
-        const course = courses.find(crs => crs.name == courseName);
-    
-        if (course)
-          reject("Course Already Exists");
-    
+        const codurse = courses.find((crs) => crs.name == courseName);
+        const course = courses.find(crs => {
+          let name = crs.name.split(" ");
+          name = crs.name.map(w => w.toLowerCase());
+          name = name.join("_");
+
+          return name == courseName;
+        });
+
+
+        console.log(course);
+        if (course) throw("Course Already Exists");
+
+
         // 3) Create the Questions File.
-        await fs.writeFile(path.join(questionsPath, `${courseName}.json`));
-        
+        await fs.writeFile(path.join(questionsPath, `${courseName}.json`), "[]");
+
         // 4) Add to the database.
+
         courses.push(this);
         await fs.writeFile(coursesPath, JSON.stringify(courses));
-        
+
         resolve("Course Added Successfully");
       } catch (err) {
+        console.log(err);
         reject(err);
       }
-    })
-
-
-    
-
+    });
   }
 
   static courses() {
-    return new Promise (async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       try {
         const data = await fs.readFile(coursesPath);
         const courses = JSON.parse(data);
         resolve(courses);
       } catch (err) {
-        reject (err);
+        reject(err);
       }
-    })
+    });
   }
-
-}
+};
